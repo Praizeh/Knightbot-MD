308585c753879155569ec8bb8c52af4d8da999db
Praizeh <144949572+Praizeh@users.noreply.github.com>
2025-05-27 16:41:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Femz Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Femz Bot",
   botOwner: 'Daddy Femz', // Your name
   ownerNumber: '2348068046499, 2349074285271', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
